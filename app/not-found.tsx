703151c94ce4fc0bb26c8d1ab4804d0c203bc417
Sang Hun Park <62207757+bp4sp4@users.noreply.github.com>
2025-01-13 17:08:14 +0900

    NextJS 초기세팅

---
+++ app/not-found.tsx
@@ -0,0 +1,10 @@
+import Navigation from "../components/navigation";
+
+export default function NotFount() {
+  return (
+    <div>
+      <Navigation />
+      <h1>Not Found</h1>
+    </div>
+  );
+}
